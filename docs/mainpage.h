6052b2eb8fb5f217c0ce4961d10e3d50943a2767
Den-Git-CPP <madim-neodart@rambler.ru>
2022-07-31 23:22:15 +0300

    Коррекция yml, cmake под action c boost 1.71

--- docs/mainpage.h
+++ docs/mainpage.h
@@ -1,6 +1,5 @@
 
-/*! \mainpage OTUS-CPP-Professional-HomeWork_8
- * Утилита для обнаружения файлов-дубликатов, разаработана с применением библиотеки Boost.
+/*! \mainpage OTUS-CPP-Professional-HomeWork_8 \n Утилита для обнаружения файлов-дубликатов, разаработана с применением библиотеки Boost.
  *  \n 
  * 
  */
